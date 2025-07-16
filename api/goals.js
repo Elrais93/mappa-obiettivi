69abedbecbe9a9adf08f21a7f5491b786a81c1a7
Elrais93 <andrearais@gmail.com>
2025-07-16 13:21:16 +0200

    Update goals.js

--- api/goals.js
+++ api/goals.js
@@ -1,1 +1,24 @@
+export default async function handler(req, res) {
+  const headers = {
+    "Content-Type": "application/json",
+    "X-Master-Key": "$2a$10$LfpSWnHyxka5Db1sdFyCZuIJvzxxSiFUNTWhFBGC0615h//REHCZy"
+  };
+
+  const binId = "687630a7bb9a9d26e899fdea";
+
+  const url = `https://api.jsonbin.io/v3/b/${binId}${req.method === "GET" ? "/latest" : ""}`;
+
+  try {
+    const response = await fetch(url, {
+      method: req.method,
+      headers,
+      body: req.method === "GET" ? undefined : JSON.stringify(req.body)
+    });
+
+    const data = await response.json();
+    res.status(200).json(data.record || data);
+  } catch (error) {
+    res.status(500).json({ error: "Errore nella chiamata a JSONBin", detail: error.toString() });
+  }
+}
 
